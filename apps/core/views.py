d0f1c5767aaf921d64672e9d34d46f9623de3657
vmendezrojas95 <vmendezrojas@frba.utn.edu.ar>
2024-04-10 10:03:57 -0300

    estable v1

---
+++ apps/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
